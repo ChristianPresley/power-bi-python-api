c713d97e4ee9f9165530868abb369674dc5d74e6
Christian Presley <Christian.Presley@microsoft.com>
2022-03-24 09:24:51 -0700

    Fix param for import file

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ pbi_client = PowerBIAPIClient(
 logging.basicConfig(level=logging.INFO)
 
 create_workspace = pbi_client.create_workspace('Testing Environment')
-import_pbix_file = pbi_client.import_file_into_workspace('Testing Environment', False, 'SharePointListDemo.pbix')
+import_pbix_file = pbi_client.import_file_into_workspace('Testing Environment', False, 'SharePointListDemo.pbix', 'SharePointListDemo')
 create_pipeline = pbi_client.create_pipeline('Testing Environment')
 add_user_workspace = pbi_client.add_user_to_workspace('Testing Environment', {"identifier": "admin@M365x51939963.onmicrosoft.com", "userAccessRight": "Admin", "principalType": "User"})
 add_user_pipeline = pbi_client.add_user_to_pipeline('Testing Environment', {"identifier": "admin@M365x51939963.onmicrosoft.com", "pipelineUserAccessRight": "Admin", "principalType": "User"})
