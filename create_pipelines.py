f3665cc442206e3f53615d00c79766a76c41ddd2
Christian Presley <Christian.Presley@microsoft.com>
2022-03-25 08:42:43 -0700

    Wait longer for pipeline to create

--- create_pipelines.py
+++ create_pipelines.py
@@ -13,7 +13,7 @@ pbi_client = PowerBIAPIClient(
 logging.basicConfig(level=logging.INFO)
 
 create_pipeline_dev = pbi_client.create_pipeline('Testing Environment')
-time.sleep(5)
+time.sleep(15)
 add_user_pipeline_dev = pbi_client.add_user_to_pipeline('Testing Environment', {"identifier": "admin@M365x51939963.onmicrosoft.com", "pipelineUserAccessRight": "Admin", "principalType": "User"})
 time.sleep(5)
 assign_pipeline_workspace_dev = pbi_client.assign_pipeline_workspace('Testing Environment', 'Testing Environment [Dev]', 0)
