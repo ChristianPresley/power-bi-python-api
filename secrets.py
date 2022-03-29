5ed24531f93154af8a388d40fbfd6914e044aa25
Christian Presley <Christian.Presley@microsoft.com>
2022-03-29 15:42:11 -0700

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,3 +1,5 @@
-tenant_id = 'a208fa35-44ee-4d61-9589-947f11ff7ed6'
-client_id = '4106f761-3b6c-4099-849e-31fbd638ffb0'
-client_secret = 'BjV7Q~5XrlWhi3gX.vIU27.Kk~2EDORQcdmCx'
+import os
+
+tenant_id = os.getenv('AZURE_TENANT_ID')
+client_id = os.getenv('AZURE_CLIENT_ID')
+client_secret = os.getenv('AZURE_CLIENT_SECRET')
